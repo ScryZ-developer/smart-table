3ad2bc23cceed1b7ebc96b3ac1efa7bd75a0586e
Ilya Lonshakov <modnyi-l@mail.ru>
2025-11-09 12:16:55 +0700

    feat: all files are connected

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
 // @todo: подключение
+import {initSearching} from "./components/searching.js";
+import {initFiltering} from "./components/filtering.js";
+import {initSorting} from "./components/sorting.js";
+import {initPagination} from "./components/pagination.js";
 
 
 // Исходные данные используемые в render()
