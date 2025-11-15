3003b6cf22d8b3a68447aacfc43ff6a9fb5338b8
Ilya Lonshakov <modnyi-l@mail.ru>
2025-11-15 17:28:48 +0700

    final

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,23 +1,17 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }))
+        })
+    }
 
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
-
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes).forEach(elementName => {
-        elements[elementName].append(
-            ...Object.values(indexes[elementName]).map(name => {
-                const option = document.createElement('option');
-                option.value = name;
-                option.textContent = name;
-                return option;
-            })
-        );
-    });
-
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+    const applyFiltering = (query, state, action) => {
+        // Обработка очистки поля
         if (action && action.name === 'clear') {
             const field = action.dataset.field;
             const parent = action.closest('.field');
@@ -28,7 +22,21 @@ export function initFiltering(elements, indexes) {
             }
         }
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+        // Формирование параметров фильтрации для сервера
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
+                    filter[`filter[${elements[key].name}]`] = elements[key].value;
+                }
+            }
+        });
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
     }
+
+    return {
+        updateIndexes,
+        applyFiltering
+    };
 }
