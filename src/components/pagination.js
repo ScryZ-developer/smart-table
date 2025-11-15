339ff11892fc5d82307187f56fc6124996cced93
Ilya Lonshakov <modnyi-l@mail.ru>
2025-11-15 17:23:18 +0700

    final

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,3 +1,5 @@
+import {getPages} from "../lib/utils.js";
+
 export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) => {
     const pageTemplate = pages.firstElementChild.cloneNode(true);
     pages.firstElementChild.remove();
